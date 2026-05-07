88af8e31b6734d96e6202283191ccb66a034a136
Samuel H.Mariam <samhmariam@gmail.com>
2026-05-07 12:26:33 +0100

    Fix deprecated agent property; add workflow and agent integration modules

---
+++ Develop AI agents on Azure/08-build-workflow-ms-foundry/workflow.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+
+# Add references
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+
+
+load_dotenv()
+endpoint = os.environ["PROJECT_ENDPOINT"]
+
+# Connect to the AI Project client
+with (
+    DefaultAzureCredential() as credential,
+    AIProjectClient(endpoint=endpoint, credential=credential) as project_client,
+    project_client.get_openai_client() as openai_client,
+):
+
+
+    # Specify the workflow
+    workflow = {
+        "name": "ContosoPay-Customer-Support-Triage"
+    }
+    
+
+    # Create a conversation and run the workflow
+    conversation = openai_client.conversations.create()
+    print(f"Created conversation (id: {conversation.id})")
+
+    stream = openai_client.responses.create(
+        conversation=conversation.id,
+        extra_body={"agent_reference" : {"name" : workflow["name"], "type": "agent_reference"}},
+        input="Start",
+        stream=True,
+    )
+
+
+     # Process events from the workflow run
+    for event in stream:
+        if (event.type == "response.completed"):
+            print("\nResponse completed:")
+            response = openai_client.responses.retrieve(event.response.id)
+            print(f"{response.output_text}")
+ 
+
+    # Clean up resources
+    openai_client.conversations.delete(conversation_id=conversation.id)
+    print("\nConversation deleted")
